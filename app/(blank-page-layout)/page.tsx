734cd4a60d14cb0d4da444871df7a7e48508a11a
mojahidalamcse <mojahidalamcse@gmail.com>
2026-07-18 03:25:56 +0600

    feat(home): update HomeView import path and remove SkillPage component

--- app/(blank-page-layout)/page.tsx
+++ app/(blank-page-layout)/page.tsx
@@ -1,10 +1,12 @@
 import { Container } from "@/components/shared/container";
-import { HomeView } from "@/views/home";
+import { HomeView } from "@/views/home/index";
 
 const HomePage = () => {
   return (
-    <Container className="py-20">
+    <Container className="py-20">  
+
       <HomeView />
+
     </Container>
   );
 };
